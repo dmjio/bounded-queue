db9f20716aef076f0090767c07668d859e850ca7
David Johnson <djohnson.m@gmail.com>
2020-07-14 13:57:37 -0400

    Rely on Seq.length.

--- src/Data/BQueue.hs
+++ src/Data/BQueue.hs
@@ -34,36 +34,33 @@ import           Prelude       hiding (seq)
 -- | Bound information, specified by user
 type Bound = Int
 
--- | Size information, used internally
-type Size   = Int
-
 -- | Bounded Queue implementation
-data BQueue a = BQueue !Bound !Size !(Seq a)
+data BQueue a = BQueue !Bound !(Seq a)
   deriving (Show, Eq, Typeable, Generic, Data)
 
 instance Semigroup (BQueue a) where
-  BQueue bound1 size1 s1 <> BQueue bound2 size2 s2 =
-    BQueue (bound1 + bound2) (size1 + size2) (s1 <> s2)
+  BQueue bound1 s1 <> BQueue bound2 s2 =
+    BQueue (bound1 + bound2) (s1 <> s2)
 
 instance Monoid (BQueue a) where
   mempty = mempty
 
 instance Functor BQueue where
-  fmap f (BQueue bound sz s) = BQueue bound sz (fmap f s)
+  fmap f (BQueue bound s) = BQueue bound (fmap f s)
 
 instance Foldable BQueue where
-  foldr f x (BQueue _ _ s) = foldr f x s
+  foldr f x (BQueue _ s) = foldr f x s
 
 instance Traversable BQueue where
-  traverse f (BQueue bound sz s) = BQueue bound sz <$> traverse f s
+  traverse f (BQueue bound s) = BQueue bound <$> traverse f s
 
 -- | Construct an empty 'BQueue', /O(1)/
 empty :: Bound -> BQueue a
-empty bound = BQueue bound 0 mempty
+empty bound = BQueue bound mempty
 
 -- | Convert a 'BQueue' to a list, /O(n)/
 toList :: BQueue a -> [a]
-toList (BQueue _ _ s) = F.toList s
+toList (BQueue _ s) = F.toList s
 
 -- | Construct a 'BQueue' from a list, /O(n)/
 fromList :: Int -> [a] -> BQueue a
@@ -74,22 +71,22 @@ fromList bound =
 -- | Enqueue an item onto a 'BQueue', /O(1)/
 -- Returns an item if the bound has been exceeded
 enqueue :: a -> BQueue a -> (Maybe a, BQueue a)
-enqueue x (BQueue bound size xs)
-  | size >= bound =
+enqueue x (BQueue bound xs)
+  | S.length xs >= bound =
      case S.viewr xs of
        ys S.:> leftOver ->
-         (Just leftOver, BQueue bound size (ys S.|> x))
+         (Just leftOver, BQueue bound (ys S.|> x))
        S.EmptyR ->
-         (Nothing, BQueue bound size mempty)
+         (Nothing, BQueue bound mempty)
   | otherwise =
-      (Nothing, BQueue bound (size + 1) (xs S.|> x))
+      (Nothing, BQueue bound (xs S.|> x))
 
 -- | Dequeue an item from a 'BQueue', /O(1)/
 -- Returns the item along with the updated 'BQueue'
 dequeue :: BQueue a -> (Maybe a, BQueue a)
-dequeue (BQueue bound size s)
-  | S.null s = (Nothing, BQueue bound size mempty)
+dequeue (BQueue bound s)
+  | S.null s = (Nothing, BQueue bound mempty)
   | otherwise =
     case S.viewl s of
-      y S.:< ys -> (Just y, BQueue bound (size - 1) ys)
-      S.EmptyL -> (Nothing, BQueue bound size s)
+      y S.:< ys -> (Just y, BQueue bound ys)
+      S.EmptyL -> (Nothing, BQueue bound s)
