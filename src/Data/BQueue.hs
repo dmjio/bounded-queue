060b35002efcf839d596b0d801f6f45dd062007a
dmjio <code@dmj.io>
2024-12-13 18:47:53 -0600

    update copyright, maintainer

--- src/Data/BQueue.hs
+++ src/Data/BQueue.hs
@@ -8,7 +8,7 @@
 -- Module      :  Data.BQueue
 -- Copyright   :  (C) 2020 David M. Johnson
 -- License     :  BSD3-style (see the file LICENSE)
--- Maintainer  :  David M. Johnson <djohnson.m@gmail.com>
+-- Maintainer  :  David M. Johnson <code@dmj.io>
 -- Stability   :  experimental
 -- Portability :  non-portable
 ----------------------------------------------------------------------------
